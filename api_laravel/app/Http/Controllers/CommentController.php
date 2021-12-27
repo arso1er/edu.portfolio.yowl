37f4fb19ece3d0180f8ffce2e8fd78af92dfc1f6
Jefferson <jefferson-arsene-minakpon.vodounnou@epitech.eu>
2021-12-27 19:12:17 +0100

    API auth and comment CRUD first milestone reached. Before avg and group by in comment search.

---
+++ api_laravel/app/Http/Controllers/CommentController.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\Comment;
+
+use DB;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        // Get params
+        $site = $request->query('site', '') ?? '';
+        $stars = $request->query('stars', '') ?? '';
+        $user = $request->query('user', '') ?? '';
+        $sort = $request->query('sort', 'id desc') ?? 'id desc';
+
+        $stars_str = trim($stars) === "" ? "" : "AND rating IN ($stars)";
+        $user_str = trim($user) === "" ? "" : "AND user_id = $user";
+        // var_dump($stars_str);  // OR site_name LIKE \"%$site%\"
+
+        // Count query
+        $countStdClass = DB::select(DB::raw("
+            SELECT COUNT(*) as total FROM comments
+            WHERE site_link LIKE \"%$site%\"
+            $stars_str
+            $user_str
+            ORDER BY $sort
+        "));
+        $count = (array) $countStdClass[0];
+
+        // $startAt; $perPage; $title; $category; $sort; $min_price; $max_price;
+        $perPage = $request->query('per_page', '10');
+        $page = $request->query('page', 1);
+        $startAt = $perPage * ($page - 1);
+        $totalPages = ceil($count['total'] / $perPage);
+
+        $comments = DB::select(DB::raw("
+            SELECT * FROM comments
+            WHERE site_link LIKE \"%$site%\"
+            $stars_str
+            $user_str
+            ORDER BY $sort
+            LIMIT $startAt, $perPage
+        "));
+
+        $response = [
+            'status' => 'success',
+            'comments' => $comments,
+            'perPage' => $perPage,
+            'page' => $page,
+            'startAt' => $startAt,
+            'totalPages' => $totalPages,
+            'total' => $count['total'],
+        ];
+
+        return response($response, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $fields = $request->validate([
+            'site_link' => ['required', 'string', 'max:255'],
+            'site_name' => ['required', 'string', 'max:255'],
+            'rating' => ['required', 'numeric', 'min:1', 'max:5'],
+            'title' => ['required', 'string', 'max:255'],
+            'content' => ['required', 'string', 'min:5'],
+        ]);
+
+        $user = $request->user();
+
+        $comment = Comment::create([
+            'site_link' => $fields['site_link'],
+            'site_name' => $fields['site_name'],
+            'rating' => $fields['rating'],
+            'title' => $fields['title'],
+            'content' => $fields['content'],
+            'user_id' => $user->id
+        ]);
+
+        $response = [
+            'status' => 'success',
+            'message' => 'The comment has been created successfully!',
+            'comment' => $comment
+        ];
+
+        return response($response, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $comment = Comment::where('id', $id)
+                            ->first();
+
+        if(!isset($comment)) {
+            return response()->json(['error' => 'Not Found'], 404);
+        }
+        return response([
+            'status' => 'success',
+            'comment' => $comment
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $comment = Comment::where('id', $id)
+                            ->first();
+
+        if(!isset($comment)) {
+            return response()->json(['error' => 'Not Found'], 404);
+        }
+
+        $user = $request->user();
+        if($user->id !== $comment->user_id && $user->role !== 'admin') {
+            return response()->json(['error' => 'You are not allowed to do that!'], 403);
+        }
+
+        $fields = $request->validate([
+            'site_link' => ['required', 'string', 'max:255'],
+            'site_name' => ['required', 'string', 'max:255'],
+            'rating' => ['required', 'numeric', 'min:1', 'max:5'],
+            'title' => ['required', 'string', 'max:255'],
+            'content' => ['required', 'string', 'min:5'],
+        ]);
+        // https://stackoverflow.com/a/62099533
+        $comment = tap(Comment::where('id', $id))
+                    ->update([
+                        'site_link' => $fields['site_link'],
+                        'site_name' => $fields['site_name'],
+                        'rating' => $fields['rating'],
+                        'title' => $fields['title'],
+                        'content' => $fields['content'],
+                    ])
+                    ->first();
+        $response = [
+            'status' => 'success',
+            'message' => 'The comment has been updated successfully!',
+            'comment' => $comment
+        ];
+        return response($response, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, $id)
+    {
+        $comment = Comment::where('id', $id)
+                            ->first();
+
+        if(!isset($comment)) {
+            return response()->json(['error' => 'Not Found'], 404);
+        }
+
+        $user = $request->user();
+        if($user->id !== $comment->user_id && $user->role !== 'admin') {
+            return response()->json(['error' => 'You are not allowed to do that!'], 403);
+        }
+
+        Comment::where('id', $id)->delete();
+        $response = [
+            'status' => 'success',
+            'message' => 'The comment has been deleted successfully!'
+        ];
+        return response($response, 200);
+    }
+}
--- api_laravel/routes/api.php
+++ api_laravel/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,14 +20,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/comments', function() {
-    // return 'comments';
-
-    // Test database connection
-    try {
-        DB::connection()->getPdo();
-        die("Working");
-    } catch (\Exception $e) {
-        die("Could not connect to the database.  Please check your configuration. error:" . $e->getMessage() );
-    }
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/comments', [CommentController::class, 'store']);
+    Route::put('/comments/{id}', [CommentController::class, 'update']);
+    Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
 });
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/comments', [CommentController::class, 'index']);
+Route::get('/comments/{id}', [CommentController::class, 'show']);
