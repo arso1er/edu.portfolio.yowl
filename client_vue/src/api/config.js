0cfaca5a6686ae3e89fce9fd3f00b8b106143a2e
Jefferson <jefferson-arsene-minakpon.vodounnou@epitech.eu>
2022-01-05 00:57:41 +0100

    Added env check if api config vuejs.

--- client_vue/src/api/config.js
+++ client_vue/src/api/config.js
@@ -1,4 +1,8 @@
 // https://serversideup.net/build-an-api-wrapper-with-vuejs-axios/
 export default {
-  apiBaseURL: "http://127.0.0.1:8001/api",
+  // apiBaseURL: "http://127.0.0.1:8001/api",
+  apiBaseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.VUE_APP_API_BASEURL_PROD + "/api"
+      : process.env.VUE_APP_API_BASEURL + "/api",
 };
